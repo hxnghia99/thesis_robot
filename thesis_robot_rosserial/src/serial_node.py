5d17b17d9a0aa719cad29438a59b74d9ee8ab863
hxnghia99 <hxnghia99@gmail.com>
2021-04-24 10:08:01 +0700

    install package rosserial, built with python3 (including geometry2_msgs)

---
+++ thesis_robot_rosserial/src/serial_node.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+######################################
+# file serial_node.py
+# Author:   HXNghia
+# Date:     April 23th, 2021
+######################################
+
+import rospy
+from rosserial_client import SerialClient
+from serial import SerialException
+from time import sleep
+import multiprocessing
+import sys
+sys.dont_write_bytecode = True
+
+if __name__=="__main__":
+    rospy.init_node("serial_node")
+    rospy.loginfo("ROS Serial Python Node")
+
+    port_name = rospy.get_param('~port','/dev/ttyUSB0')
+    baud = int(rospy.get_param('~baud','115200'))
+
+    fix_pyserial_for_test = rospy.get_param('~fix_pyserial_for_test', False)
+
+            # Use serial port
+    while not rospy.is_shutdown():
+        rospy.loginfo("Connecting to %s at %d baud" % (port_name,baud) )
+        try:
+            print("Trying .............")
+            client = SerialClient(port_name, baud, fix_pyserial_for_test=fix_pyserial_for_test)
+            client.run()
+        except KeyboardInterrupt:
+            break
+        except SerialException:
+            sleep(1.0)
+            continue
+        except OSError:
+            sleep(1.0)
+            continue
+        except:
+            rospy.logwarn("Unexpected Error: %s", sys.exc_info()[0])
+            client.port.close()
+            sleep(1.0)
+            continue
